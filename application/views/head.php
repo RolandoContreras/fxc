84dd52c16c46c470dea827d78c41c8ac42b148b0
unknown <RolandoContreras>
2019-10-24 17:28:28 -0500

    create login, and template

---
+++ application/views/head.php
@@ -0,0 +1,9 @@
+<head>
+  <meta charset="UTF-8">
+  <meta name="description" content="">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Rolando Contreras - El Arte de cambiar vidas</title>
+  <link rel="icon" href="./img/core-img/favicon.png">
+  <link rel="stylesheet" href="<?php echo site_url().'static/page_front/css/styles.css';?>">
+</head>
---
+++ application/views/login.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="au theme template">
+  <meta name="author" content="Hau Nguyen">
+  <meta name="keywords" content="au theme template">
+  <title>Inicio de Sesión</title>
+  <link href="<?php echo site_url().'static/page_front/css/login/font-face.css';?>" rel="stylesheet" media="all">
+  <link href="<?php echo site_url().'static/page_front/css/login/font-awesome.min.css';?>" rel="stylesheet" media="all">
+  <link href="<?php echo site_url().'static/page_front/css/login/fontawesome-all.min.css';?>" rel="stylesheet" media="all">
+  <link href="<?php echo site_url().'static/page_front/css/login/material-design-iconic-font.min.css';?>" rel="stylesheet" media="all">
+  <link href="<?php echo site_url().'static/page_front/css/login/bootstrap.min.css';?>" rel="stylesheet" media="all">
+  <link href="<?php echo site_url().'static/page_front/css/login/animsition.min.css';?>" rel="stylesheet" media="all">
+  <link href="<?php echo site_url().'static/page_front/css/login/bootstrap-progressbar-3.3.4.min.css';?>" rel="stylesheet" media="all">
+  <link href="<?php echo site_url().'static/page_front/css/login/animate.css';?>" rel="stylesheet" media="all">
+  <link href="<?php echo site_url().'static/page_front/css/login/hamburgers.min.css';?>" rel="stylesheet" media="all">
+  <link href="<?php echo site_url().'static/page_front/css/login/slick.css';?>" rel="stylesheet" media="all">
+  <link href="<?php echo site_url().'static/page_front/css/login/select2.min.css';?>" rel="stylesheet" media="all">
+  <link href="<?php echo site_url().'static/page_front/css/login/perfect-scrollbar.css';?>" rel="stylesheet" media="all">
+  <link href="<?php echo site_url().'static/page_front/css/login/theme.css';?>" rel="stylesheet" media="all">
+  <!--https://colorlib.com/polygon/cooladmin/-->
+</head>
+
+<body class="animsition">
+  <div class="page-wrapper">
+    <div class="page-content--bge5">
+      <div class="container">
+        <div class="login-wrap">
+          <div class="login-content">
+            <div class="login-logo">
+                <!--LOGO-->
+              <a href="#">
+                  <img src="<?php echo site_url().'static/page_front/images/logo/logo.jpg';?>" alt="Empire" width="100">
+              </a>
+            </div>
+            <div class="login-form">
+              <form action="" method="post">
+                <div class="form-group"><label>Usuario</label><input class="au-input au-input--full" type="email" name="email" placeholder="Email"></div>
+                <div class="form-group"><label>Contraseña</label><input class="au-input au-input--full" type="password" name="password" placeholder="Password"></div>
+                <div class="login-checkbox"><label><input type="checkbox" name="remember">Remember Me</label><label><a href="#">Forgotten Password?</a></label></div><button class="au-btn au-btn--block au-btn--green m-b-20"
+                  type="submit">sign in</button>
+                <div class="social-login-content">
+                  <div class="social-button"><button class="au-btn au-btn--block au-btn--blue m-b-20">sign in with facebook</button><button class="au-btn au-btn--block au-btn--blue2">sign in with twitter</button></div>
+                </div>
+              </form>
+              <div class="register-link">
+                <p>Don't you have account?<a href="#">Sign Up Here</a></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <script src="<?php echo site_url().'static/page_front/js/login/jquery-3.2.1.min.js';?>" type="27d243113242fdd8b9f794f9-text/javascript"></script>
+  <script src="<?php echo site_url().'static/page_front/js/login/popper.min.js';?>" type="27d243113242fdd8b9f794f9-text/javascript"></script>
+  <script src="<?php echo site_url().'static/page_front/js/login/bootstrap.min.js';?>" type="27d243113242fdd8b9f794f9-text/javascript"></script>
+  <script src="<?php echo site_url().'static/page_front/js/login/slick.min.js';?>" type="27d243113242fdd8b9f794f9-text/javascript"></script>
+  <script src="<?php echo site_url().'static/page_front/js/login/wow.min.js';?>" type="27d243113242fdd8b9f794f9-text/javascript"></script>
+  <script src="<?php echo site_url().'static/page_front/js/login/animsition.min.js';?>" type="27d243113242fdd8b9f794f9-text/javascript"></script>
+  <script src="<?php echo site_url().'static/page_front/js/login/bootstrap-progressbar.min.js';?>" type="27d243113242fdd8b9f794f9-text/javascript"></script>
+  <script src="<?php echo site_url().'static/page_front/js/login/jquery.waypoints.min.js';?>" type="27d243113242fdd8b9f794f9-text/javascript"></script>
+  <script src="<?php echo site_url().'static/page_front/js/login/jquery.counterup.min.js';?>" type="27d243113242fdd8b9f794f9-text/javascript"></script>
+  <script src="<?php echo site_url().'static/page_front/js/login/circle-progress.min.js';?>" type="27d243113242fdd8b9f794f9-text/javascript"></script>
+  <script src="<?php echo site_url().'static/page_front/js/login/perfect-scrollbar.js';?>" type="27d243113242fdd8b9f794f9-text/javascript"></script>
+  <script src="<?php echo site_url().'static/page_front/js/login/Chart.bundle.min.js';?>" type="27d243113242fdd8b9f794f9-text/javascript"></script>
+  <script src="<?php echo site_url().'static/page_front/js/login/select2.min.js';?>" type="27d243113242fdd8b9f794f9-text/javascript"></script>
+  <script src="<?php echo site_url().'static/page_front/js/login/main.js';?>" type="27d243113242fdd8b9f794f9-text/javascript"></script>
+  <script src="https://ajax.cloudflare.com/cdn-cgi/scripts/95c75768/cloudflare-static/rocket-loader.min.js" data-cf-settings="27d243113242fdd8b9f794f9-|49" defer=""></script>
+</body>
+</html>
